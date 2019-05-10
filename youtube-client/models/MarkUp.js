75e11147ca7dea515b4cb86588653298fe185d40
deminiro <den.mironov2001@yandex.by>
2019-05-10 16:02:05 +0300

    feat: add information from video without amount of viewers

--- youtube-client/models/MarkUp.js
+++ youtube-client/models/MarkUp.js
@@ -1,5 +1,4 @@
-// import qw from './Requests';
-const arr = require('./Requests');
+// import * as qw from './Requests';
 
 
 export default function MarkUp() {
@@ -26,9 +25,6 @@ export default function MarkUp() {
 
     const query = document.getElementById('query');
     const submit = document.getElementById('submit');
-    submit.addEventListener('click', () => {
-      localStorage.setItem('query', query.value);
-    });
     // input with text
     query.style.width = '70%';
     query.style.fontSize = '20px';
@@ -42,7 +38,8 @@ export default function MarkUp() {
     submit.style.border = 'none';
     submit.style.position = 'relative';
     submit.style.left = '5%';
-    global.console.log(arr.getUsers());
+    // global.console.log(qw.default.extractClipNames());
+    // const p = new Promise((resolve));
   }
 
   async function clipComponents() {
@@ -62,8 +59,9 @@ export default function MarkUp() {
     const midRightBox = document.getElementById('middle-right-box');
     const rightBox = document.getElementById('right-box');
     (function leftComponent() {
-      leftBox.setAttribute('style', 'display: grid; grid-template-rows: 35% 11% 11% 11% 32%; background: white; border-radius: 2%;');
-      leftBox.innerHTML += '<div id="left-box-head" class="clip-components-head" style="width: 100%"></div>';
+      leftBox.setAttribute('style', 'display: grid; grid-template-rows: 9% 35% 8% 8% 10% 30%; background: white; border-radius: 2%;');
+      leftBox.innerHTML += '<div id="left-box-headline" class="clip-components-headline" style="width: 100%"></div>';
+      leftBox.innerHTML += '<div id="left-box-preview" class="clip-components-preview" style="width: 100%"></div>';
       leftBox.innerHTML += '<div id="left-box-title" class="clip-components-title" style="width: 100%"></div>';
       leftBox.innerHTML += '<div id="left-box-date" class="clip-components-date" style="width: 100%"></div>';
       leftBox.innerHTML += '<div id="left-box-viewers" class="clip-components-viewers" style="width: 100%"></div>';
@@ -71,8 +69,9 @@ export default function MarkUp() {
     }());
 
     (function midLeftComponent() {
-      midLeftBox.setAttribute('style', 'display: grid; grid-template-rows: 35% 11% 11% 11% 32%; background: white; border-radius: 2%;');
-      midLeftBox.innerHTML += '<div id="mid-left-box-head" class="clip-components-head" style="width: 100%"></div>';
+      midLeftBox.setAttribute('style', 'display: grid; grid-template-rows: 9% 35% 8% 8% 10% 30%; background: white; border-radius: 2%;');
+      midLeftBox.innerHTML += '<div id="mid-left-box-headline" class="clip-components-headline" style="width: 100%"></div>';
+      midLeftBox.innerHTML += '<div id="mid-left-box-preview" class="clip-components-preview" style="width: 100%"></div>';
       midLeftBox.innerHTML += '<div id="mid-left-box-title" class="clip-components-title" style="width: 100%"></div>';
       midLeftBox.innerHTML += '<div id="mid-left-box-date" class="clip-components-date" style="width: 100%"></div>';
       midLeftBox.innerHTML += '<div id="mid-left-box-viewers" class="clip-components-viewers" style="width: 100%"></div>';
@@ -80,8 +79,9 @@ export default function MarkUp() {
     }());
 
     (function leftComponent() {
-      midRightBox.setAttribute('style', 'display: grid; grid-template-rows: 35% 11% 11% 11% 32%; background: white; border-radius: 2%;');
-      midRightBox.innerHTML += '<div id="mid-right-box-head" class="clip-components-head" style="width: 100%"></div>';
+      midRightBox.setAttribute('style', 'display: grid; grid-template-rows: 9% 35% 8% 8% 10% 30%; background: white; border-radius: 2%;');
+      midRightBox.innerHTML += '<div id="mid-right-box-headline" class="clip-components-headline" style="width: 100%"></div>';
+      midRightBox.innerHTML += '<div id="mid-right-box-preview" class="clip-components-preview" style="width: 100%"></div>';
       midRightBox.innerHTML += '<div id="mid-right-box-title" class="clip-components-title" style="width: 100%"></div>';
       midRightBox.innerHTML += '<div id="mid-right-box-date" class="clip-components-date" style="width: 100%"></div>';
       midRightBox.innerHTML += '<div id="mid-right-box-viewers" class="clip-components-viewers" style="width: 100%"></div>';
@@ -89,8 +89,9 @@ export default function MarkUp() {
     }());
 
     (function leftComponent() {
-      rightBox.setAttribute('style', 'display: grid; grid-template-rows: 35% 11% 11% 11% 32%; background: white; border-radius: 2%;');
-      rightBox.innerHTML += '<div id="right-box-head" class="clip-components-head" style="width: 100%"></div>';
+      rightBox.setAttribute('style', 'display: grid; grid-template-rows: 9% 35% 8% 8% 10% 30%; background: white; border-radius: 2%;');
+      rightBox.innerHTML += '<div id="right-box-headline" class="clip-components-headline" style="width: 100%"></div>';
+      rightBox.innerHTML += '<div id="right-box-preview" class="clip-components-preview" style="width: 100%"></div>';
       rightBox.innerHTML += '<div id="right-box-title" class="clip-components-title" style="width: 100%"></div>';
       rightBox.innerHTML += '<div id="right-box-date" class="clip-components-date" style="width: 100%"></div>';
       rightBox.innerHTML += '<div id="right-box-viewers" class="clip-components-viewers" style="width: 100%"></div>';
@@ -98,32 +99,32 @@ export default function MarkUp() {
     }());
 
     (function styleClipComponents() {
-      const clipComponentsHead = Array.from(document.getElementsByClassName('clip-components-head'));
+      const clipComponentsHead = Array.from(document.getElementsByClassName('clip-components-headline'));
       const clipComponentsTitle = Array.from(document.getElementsByClassName('clip-components-title'));
       const clipComponentsDate = Array.from(document.getElementsByClassName('clip-components-date'));
       const clipComponentsViewers = Array.from(document.getElementsByClassName('clip-components-viewers'));
       const clipComponentsDescription = Array.from(document.getElementsByClassName('clip-components-description'));
 
-      clipComponentsHead.forEach((e) => {
-        e.style.background = 'red';
+      clipComponentsHead.forEach(() => {
+        // e.style.background = 'red';
       });
       clipComponentsTitle.forEach((e) => {
         e.style.display = 'grid';
-        e.style.gridTemplateColumns = '35% 65%';
+        e.style.gridTemplateColumns = '15% 80%';
         e.innerHTML += '<i class="fas fa-male"></i>';
         e.style.paddingTop = '7%';
         e.style.paddingLeft = '9%';
       });
       clipComponentsDate.forEach((e) => {
         e.style.display = 'grid';
-        e.style.gridTemplateColumns = '35% 65%';
+        e.style.gridTemplateColumns = '15% 80%';
         e.innerHTML += '<i class="fas fa-calendar-alt"></i>';
         e.style.paddingTop = '7%';
         e.style.paddingLeft = '7%';
       });
       clipComponentsViewers.forEach((e) => {
         e.style.display = 'grid';
-        e.style.gridTemplateColumns = '35% 65%';
+        e.style.gridTemplateColumns = '15% 80%';
         e.innerHTML += '<i class="fas fa-eye"></i>';
         e.style.paddingTop = '7%';
         e.style.paddingLeft = '6%';
--- youtube-client/models/Requests.js
+++ youtube-client/models/Requests.js
@@ -8,31 +8,58 @@ export default class AppModel {
     const channelTitle = data.items.map(clip => clip.snippet.channelTitle);
     const time = data.items.map(clip => clip.snippet.publishedAt);
     const description = data.items.map(clip => clip.snippet.description);
-    const image = data.items.map(clip => clip.snippet.thumbnails);
+    const image = data.items.map(clip => clip.snippet.thumbnails.medium.url);
+    const amountOfResults = data.pageInfo.totalResults;
+    // global.console.log(data.items.map(clip => clip.snippet.thumbnails));
     const arr = [];
-    module.exports = {
-      getUsers: function qwe() {
-        return arr;
-      },
-    };
-
-    for (let i = 0; i < 4; i += 1) {
-      const img = image[i].default.url;
+    for (let i = 0; i < 4 || i < amountOfResults; i += 1) {
+      const img = image[i];
       const ttl = title[i];
       const chTtl = channelTitle[i];
-      const tim = time[i];
+      const dateUpload = time[i];
       const descr = description[i];
       arr.push({
-        img, ttl, chTtl, tim, descr,
+        img, ttl, chTtl, dateUpload, descr,
       });
     }
-    global.console.log(arr);
-    return arr;
+    setTimeout(() => {
+      const channelHeadline = document.getElementsByClassName('clip-components-headline');
+      const channelName = document.getElementsByClassName('clip-components-title');
+      const clipDate = document.getElementsByClassName('clip-components-date');
+      const preview = document.getElementsByClassName('clip-components-preview');
+      const clipDescription = document.getElementsByClassName('clip-components-description');
+
+      for (let j = 0; j < channelName.length; j += 1) {
+        channelHeadline[j].innerHTML += arr[j].ttl;
+        channelHeadline[j].style.background = 'lightgreen';
+
+        preview[j].style.background = `url('${arr[j].img}') no-repeat`;
+        preview[j].style.backgroundSize = '100%';
+
+        channelName[j].innerHTML += `<div>${arr[j].chTtl}</div>`;
+
+        const timeOfUpload = new Date(arr[j].dateUpload);
+        const fullYear = timeOfUpload.getFullYear();
+        const month = timeOfUpload.getMonth();
+        const correctMonth = Number(month);
+        const day = timeOfUpload.getDate();
+        clipDate[j].innerHTML += `${fullYear}-${day}-${correctMonth + 1}`;
+
+        clipDescription[j].innerHTML += arr[j].descr;
+      }
+    }, 100);
   }
 
+
   async getClipNames() {
-    const storage = localStorage.getItem('query');
-    const url = this.state.url.concat(storage);
+    // const submit = document.getElementById('submit');
+    // const query = document.getElementById('query');
+    const { url } = this.state;
+    // submit.addEventListener('click', () => {
+    //   global.console.log(query.value);
+    //   url.concat(query.value);
+    // });
+
     const response = await fetch(url);
     // global.console.log(url);
     const data = await response.json();
